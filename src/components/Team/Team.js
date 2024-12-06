cc6f68244934e7b36dff36ba5e53d6135b9fdd41
Samilly <samillynunes19@gmail.com>
2024-12-05 23:08:27 -0300

    chore: teams colors customized

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -2,8 +2,12 @@ import './Team.css';
 
 export const Team = (props) => {
     return (
-        <section className='team'>
-            <h3> {props.name} </h3>
+        <section className='team' style={{
+            backgroundColor: props.secondaryColor
+        }}>
+            <h3 style={{
+                borderColor: props.primaryColor
+            }} > {props.name} </h3>
         </section>
     );
 }
