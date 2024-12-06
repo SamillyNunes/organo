d18947d9943bb96cf12cd398d47e1404d3d7fcee
Samilly <samillynunes19@gmail.com>
2024-12-05 22:47:42 -0300

    draft: working on teams

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Banner from './components/Banner';
 import Form from './components/Form';
+import Team from './components/Team';
 
 function App() {
 
@@ -16,6 +17,9 @@ function App() {
     <div className="App">
       <Banner />
       <Form onSaveStaff={staff => saveStaff(staff)} />
+      <Team name="Programação" />
+      <Team name="Front-End" />
+      <Team name="Data Science" />
     </div>
   );
 }
